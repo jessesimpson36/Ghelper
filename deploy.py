99f33bfeaf975fad1b3c5d27f8b8ff54f29ff5ac
Jesse Simpson <jesse.simpson36@gmail.com>
2025-06-18 22:18:12 -0400

    fix: use remote pdb

--- deploy.py
+++ deploy.py
@@ -14,6 +14,7 @@ import time
 import xmlrpc.client
 import yaml
 import jinja2
+from remote_pdb import set_trace
 
 
 ###############################################################################
@@ -218,7 +219,7 @@ def boot():
 
 ###############################################################################
 ###############################################################################
-
+set_trace()
 boots = {}
 templatedir = os.getcwd()
 dtemplates_yaml = "all.yaml"
